81f77e8d58c1e410f132ae30ebec316a280c406e
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-03-01 13:43:19 +0100

    yes

--- Tests/Vulkan/main.c
+++ Tests/Vulkan/main.c
@@ -9,11 +9,13 @@ static size_t trace_count = 0;
 
 void ErrorCallback(void* data, const char* msg, int errnum)
 {
+	(void)data;
 	fprintf(stderr, "Error in backtrace: %s (error %d)\n", msg, errnum);
 }
 
 int SymbolCallback(void* data, unsigned long pc, const char* filename, int lineno, const char* function)
 {
+	(void)data;
 	if(filename && function)
 		fprintf(stderr, "\t#%zu\033[1;34m 0x%lx\033[1;0m in\033[1;33m %s() from %s:%d\033[1;0m\n", trace_count, pc, function, filename, lineno);
 	else if(function)
@@ -53,11 +55,3 @@ int main(void)
 	TestPipeline();
 	return UNITY_END();
 }
-
-/*
-#0  0x00007ffff79ad624 in ?? () from /usr/lib/libc.so.6
-#1  0x00007ffff7953ba0 in raise () from /usr/lib/libc.so.6
-#2  0x000055555560ec86 in TestPipelineReadOnlyBindings () at Tests/Vulkan/Pipeline.c:54
-#3  0x000055555560f682 in TestPipeline () at Tests/Vulkan/Pipeline.c:228
-#4  0x000055555560ea01 in main () at Tests/Vulkan/main.c:46
-*/
