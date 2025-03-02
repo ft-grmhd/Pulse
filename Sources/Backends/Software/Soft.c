ee430d0e6e0cbd90faddb69d7274c1bb232714b1
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-03-02 21:39:13 +0100

    fixing wrong variable use

--- Sources/Backends/Software/Soft.c
+++ Sources/Backends/Software/Soft.c
@@ -23,7 +23,7 @@ bool SoftLoadBackend(PulseBackend backend, PulseDebugLevel debug_level)
 	PULSE_UNUSED(debug_level);
 	SoftDriverData* driver_data = (SoftDriverData*)calloc(1, sizeof(SoftDriverData));
 	PULSE_CHECK_ALLOCATION_RETVAL(driver_data, false);
-	WebGPUDriver.driver_data = driver_data;
+	SoftwareDriver.driver_data = driver_data;
 	return true;
 }
 
